4a00c2cc392c828bf128a81e20c7a1d29f11b040
shyunju7 <shyunju7@naver.com>
2022-03-06 23:33:13 +0900

    Study lecture-q/chapter01/SevenDwarfs.js

--- javascript-algorithm/src/lecture-q/chpater01/SevenDwarfs.js
+++ javascript-algorithm/src/lecture-q/chpater01/SevenDwarfs.js
@@ -1,4 +1,5 @@
 /* 진짜 일곱 난쟁이 찾기 - 7명의 난쟁이의 키의 합은 100이다. */
+const readline = require("readline");
 
 const solution = (arr) => {
   let answer;
@@ -13,9 +14,23 @@ const solution = (arr) => {
       }
     }
   }
-
-  return answer;
+  answer.sort((a, b) => a - b);
+  answer.map((height) => console.log(height));
 };
 
-let arr = [20, 7, 23, 19, 10, 15, 25, 8, 13];
-console.log(solution(arr));
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+rl.on("line", (line) => {
+  input.push(Number(line));
+
+  if (input.length === 9) {
+    solution(input);
+  }
+});
+
+// let arr = [20, 7, 23, 19, 10, 15, 25, 8, 13];
+// console.log(solution(arr));
