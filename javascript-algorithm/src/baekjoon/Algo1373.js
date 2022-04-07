67e13b83bd64ef7699ae67d8cdf3d29a8381f410
shyunju7 <shyunju7@naver.com>
2022-04-07 15:34:35 +0900

    Solution baekjoon/Algo1373.js

--- javascript-algorithm/src/baekjoon/Algo1373.js
+++ javascript-algorithm/src/baekjoon/Algo1373.js
@@ -1,12 +1,11 @@
 /** 2진수를 8진수로 출력하는 프로그램
  * 메모리 초과 - 2진수 -> 10진수 -> 8진수
- * 시간 초과 - 해결하기..
+ * 시간 초과 - 해결하기 -> 해결!!
  * **/
 const readline = require("readline");
 
 const solution = (num) => {
   let answer = "";
-  let arr = [];
 
   while (num.length % 3 !== 0) {
     num = "0" + num;
@@ -15,23 +14,8 @@ const solution = (num) => {
   let length = num.length;
 
   for (let i = 0; i < length; ) {
-    arr.push(num.substr(i, 3));
+    answer += parseInt(num.substr(i, 3), 2).toString(8);
     i += 3;
-  }
-
-  let tmp;
-  while (arr.length !== 0) {
-    tmp = arr.shift().split("");
-    let value = 0;
-    let size = 2;
-
-    for (let i = 0; i < 3; i++) {
-      if (tmp[i] === "1") {
-        size === 0 ? (value += 1) : (value += size * 2);
-      }
-      size--;
-    }
-    answer += value;
   }
 
   return answer;
