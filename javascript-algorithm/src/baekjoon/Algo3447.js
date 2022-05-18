9109c904aa3a5598be5c3d3fd71ecd1b8ff2cb9d
shyunju7 <shyunju7@naver.com>
2022-05-18 18:27:28 +0900

    Solution baekjoon/Algo3447.js

--- javascript-algorithm/src/baekjoon/Algo3447.js
+++ javascript-algorithm/src/baekjoon/Algo3447.js
@@ -2,18 +2,15 @@
  * 백준 3447 버그왕
  * 브론즈 1
  * 문자열 파싱
- * 미해결
  */
 
-// ../../src/testFiles/baekjoon1371.txt
 const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
 const solution = (input) => {
-  const regExp = new RegExp("BUG", "gi");
   let answer = "";
 
   for (let line of input) {
     while (line.includes("BUG") != false) {
-      line = line.replace(regExp, "");
+      line = line.replace(/BUG/g, "");
     }
     answer += line + "\n";
   }
