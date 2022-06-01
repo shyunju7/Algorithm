f43a465936b19223ca7f73e0a4079eae5d60a7dd
shyunju7 <shyunju7@naver.com>
2022-06-01 22:30:04 +0900

    Solution, MergeSort baekjoon/Algo1181.js

--- javascript-algorithm/src/baekjoon/Algo1181.js
+++ javascript-algorithm/src/baekjoon/Algo1181.js
@@ -5,8 +5,27 @@
  * **/
 
 const readline = require("readline");
+
+const merge = (left, right) => {
+  let result = [];
+
+  while (left.length !== 0 && right.length !== 0) {
+    left[0] <= right[0]
+      ? result.push(left.shift())
+      : result.push(right.shift());
+  }
+  return [...result, ...left, ...right];
+};
+
+const mergeSort = (arr) => {
+  if (arr.length === 1) return arr;
+
+  const center = Math.floor(arr.length / 2);
+  return merge(mergeSort(arr.slice(0, center)), mergeSort(arr.slice(center)));
+};
+
 const solution = (input) => {
-  answer = input.sort();
+  answer = mergeSort(input);
   answer = answer.sort((a, b) => a.length - b.length);
   answer = new Set(answer);
   return [...answer].join("\n");
