a2e5d22363d7096af154d87e91c45740522ef7d9
shyunju7 <shyunju7@naver.com>
2022-08-24 01:33:38 +0900

    Add subset/index.js 부분집합

---
+++ javascript-algorithm/src/subset/index.js
@@ -0,0 +1,28 @@
+let N = 4;
+let numSet = [1, 2, 3, 4];
+let isSelected = Array(N).fill(false);
+
+// 출력 함수
+const print = (selected) => {
+  let answer = selected.map((state, index) => (state ? numSet[index] : "X"));
+  console.log(answer.join(" "));
+};
+
+// 부분집합 생성
+const subset = (idx) => {
+  // 기저조건
+  if (idx == N) {
+    print(isSelected);
+    return;
+  }
+
+  // 선택한 경우
+  isSelected[idx] = true;
+  subset(idx + 1);
+
+  // 선택하지 않은 경우
+  isSelected[idx] = false;
+  subset(idx + 1);
+};
+
+subset(0);
