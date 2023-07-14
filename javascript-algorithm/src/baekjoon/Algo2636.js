bc525d264b9444a9f7991ba40f096cea48223290
송현주 <shyunju7@naver.com>
2023-07-14 13:13:45 +0900

    [BAEKJOON] 2636_토마토 풀이

---
+++ javascript-algorithm/src/baekjoon/Algo2636.js
@@ -0,0 +1,72 @@
+const solution = (input) => {
+  const dxdy = [
+    [-1, 0],
+    [1, 0],
+    [0, -1],
+    [0, 1],
+  ];
+  const [[X, Y], ...plate] = input.map((line) => line.split(" ").map(Number));
+  const isIn = (nx, ny) => nx >= 0 && ny >= 0 && nx < X && ny < Y;
+
+  const countRemainingCheese = (plate) =>
+    plate
+      .map((line) => line.filter((item) => item === 1).length)
+      .reduce((prev, cur) => prev + cur);
+
+  // 탐색 시작
+  let time = 0;
+  let remainingCheese = countRemainingCheese(plate);
+  let cheese = 0;
+
+  // 녹일 치즈가 있는 동안 진행
+  while (remainingCheese !== 0) {
+    cheese = remainingCheese;
+    const isVisited = Array.from({ length: X }, () =>
+      Array.from({ length: Y }, () => false)
+    );
+
+    const dfs = (x, y) => {
+      isVisited[x][y] = true;
+      for (let i = 0; i < 4; i++) {
+        let nx = x + dxdy[i][0];
+        let ny = y + dxdy[i][1];
+
+        if (!isIn(nx, ny) || isVisited[nx][ny]) continue;
+
+        // 공기라면
+        if (plate[nx][ny] === 0) {
+          dfs(nx, ny);
+        }
+        if (plate[nx][ny] === 1) {
+          plate[nx][ny] = 0;
+          isVisited[nx][ny] = true;
+          remainingCheese--;
+        }
+      }
+    };
+
+    // 탐색 후, 시간 추가
+    dfs(0, 0);
+    time++;
+  }
+
+  return time + "\n" + cheese;
+};
+
+// 입력 처리
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const input = [];
+rl.on("line", (line) => {
+  input.push(line);
+  if (parseInt(input[0].split(" ")[0]) + 1 === input.length) {
+    rl.close();
+  }
+});
+
+rl.on("close", () => {
+  console.log(solution(input));
+});
